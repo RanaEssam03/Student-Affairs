5ad1b7977a705638a9d823d9c4e30d283683d95e
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2023-05-25 22:09:18 +0300

    edit view students

--- Student_Affairs/students/static/js/update_info_screen.js
+++ Student_Affairs/students/static/js/update_info_screen.js
@@ -4,11 +4,21 @@ function deleteStd(){
     document.getElementById("fields").submit();
     return;
 } 
+let flag = false ;
 
 function confirmDeletion(form){
     const reponse = confirm("Are you sure you want to delete this student?");
+    flag = true
+
     if (reponse == true){
+    
         deleteStd();
     }
  
+}
+function updateStd(event){
+    if(!flag && document.getElementById("hidden").value =='0')
+        alert("student updated")
+
+    event.preventDefault();
 }
